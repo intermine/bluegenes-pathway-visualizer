a9d21aa31b688aff8985040b341fcef91e6b480e
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-19 16:48:46 +0530

    created graph configuration according to cytoscape api to allowing it to render

--- src/components/GenePathwayNetwork.js
+++ src/components/GenePathwayNetwork.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
+import cytoscape from 'cytoscape';
+import { getGraphData, createCytoscapeConfig } from '../utils';
 
 function GenePathwayNetwork({ data }) {
-	useEffect(() => {}, [data]);
+	useEffect(() => {
+		cytoscape(createCytoscapeConfig(getGraphData(data || [])));
+	}, [data]);
 
-	return <div id="cy"></div>;
+	return <div id="cy" className="cyContainer"></div>;
 }
 
 export default GenePathwayNetwork;
---
+++ src/utils.js
@@ -0,0 +1,66 @@
+function getGraphData(data) {
+	const elements = [];
+	data.forEach(el => {
+		const { symbol, name, primaryIdentifier, organism } = el;
+		elements.push({
+			group: 'nodes',
+			data: {
+				id: el.symbol,
+				info: {
+					class: el.class,
+					symbol,
+					name,
+					shortName: organism.shortName,
+					primaryIdentifier
+				}
+			}
+		});
+		el.pathways &&
+			el.pathways.forEach(pathway => {
+				const { name, identifier } = pathway;
+				elements.push({
+					group: 'nodes',
+					data: {
+						id: identifier,
+						info: {
+							class: pathway.class,
+							name
+						}
+					}
+				});
+				elements.push({
+					group: 'edges',
+					data: {
+						target: el.symbol,
+						source: identifier
+					}
+				});
+			});
+	});
+	return elements;
+}
+
+function createCytoscapeConfig(elements) {
+	return {
+		container: document.getElementById('cy'),
+		elements: elements,
+		grabbable: true,
+		style: [
+			{
+				selector: 'node',
+				style: {
+					label: 'data(id)',
+					'background-color': 'data(bg)'
+				}
+			},
+			{
+				selector: 'edge',
+				style: {
+					'line-color': '#ccc'
+				}
+			}
+		]
+	};
+}
+
+export { getGraphData, createCytoscapeConfig };
