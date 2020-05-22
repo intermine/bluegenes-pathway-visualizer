051dcdfc347cb50ebc353629b1f4a42eab4b5b0b
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-22 16:31:09 +0530

    filtered unique pathway names, changed format to a list of genes

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -4,27 +4,40 @@ import GenePathwayNetwork from './components/GenePathwayNetwork';
 
 const RootContainer = ({ serviceUrl, entity }) => {
 	const [data, setData] = useState([]);
+	const [pathwayList, setPathwayList] = useState([]);
+	const [loading, setLoading] = useState(true);
+
 	useEffect(() => {
+		setLoading(true);
 		queryData({
 			serviceUrl: serviceUrl,
 			geneId: entity.value
 		}).then(data => {
 			setData(data);
+			setLoading(false);
 		});
 	}, []);
 
+	useEffect(() => {
+		const pathways = data.reduce(
+			(acc, d) => acc.concat(d.pathways.map(p => p.name)),
+			[]
+		);
+		setPathwayList([...new Set(pathways)]);
+	}, [data]);
+
 	return (
 		<div className="rootContainer">
-			<div className="innerContainer">
-				<div className="graph">
-					<span className="chart-title">Pathway Network</span>
-					{data.length ? (
+			{loading && !pathwayList.length ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className="innerContainer">
+					<div className="graph">
+						<span className="chart-title">Pathway Network</span>
 						<GenePathwayNetwork data={data} />
-					) : (
-						<h1>Loading...</h1>
-					)}
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
