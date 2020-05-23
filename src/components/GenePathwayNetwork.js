62a61f5b98e5c4638d0e2b06e2887eb5e827ceb6
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-05-23 14:41:58 +0530

    created tooltip using DOM manipulation. showing on node hover

--- src/components/GenePathwayNetwork.js
+++ src/components/GenePathwayNetwork.js
@@ -1,13 +1,28 @@
 import React, { useEffect } from 'react';
 import cytoscape from 'cytoscape';
 import coseBilkent from 'cytoscape-cose-bilkent';
-import { getGraphData, createCytoscapeConfig } from '../utils';
+import {
+	getGraphData,
+	createCytoscapeConfig,
+	createTooltip,
+	createTooltipData
+} from '../utils';
 
 cytoscape.use(coseBilkent);
 
 function GenePathwayNetwork({ data }) {
 	useEffect(() => {
-		cytoscape(createCytoscapeConfig(getGraphData(data || [])));
+		let cy = cytoscape(createCytoscapeConfig(getGraphData(data || [])));
+		let div;
+		let node = cy.elements().nodes();
+		node.unbind('mouseover');
+		node.bind('mouseover', event => {
+			div = createTooltip(event.renderedPosition, createTooltipData(event));
+		});
+		node.unbind('mouseout');
+		node.bind('mouseout', () => {
+			div.style.display = 'none';
+		});
 	}, [data]);
 
 	return <div id="cy" className="cyContainer"></div>;
--- src/utils.js
+++ src/utils.js
@@ -75,4 +75,54 @@ function createCytoscapeConfig(elements) {
 	};
 }
 
-export { getGraphData, createCytoscapeConfig };
+function createTooltip(position, content) {
+	const div = document.createElement('div');
+	const rootElem = document.getElementsByClassName('rootContainer')[0];
+	if (rootElem.offsetWidth - position.x > 300) position.x += 50;
+	else position.x -= 350;
+	div.style.background = 'gray';
+	div.style.position = 'absolute';
+	div.style.color = 'white';
+	div.innerHTML = content;
+	div.style.top = position.y + 'px';
+	div.style.fontFamily = 'arial';
+	div.style.left = position.x + 'px';
+	div.style.padding = '10px';
+	div.style.width = '250px';
+	div.style.border = '1px solid black';
+	div.style.borderRadius = '8px';
+	document.body.append(div);
+	return div;
+}
+
+function createTooltipData(node) {
+	const {
+		data: { info }
+	} = node.target[0]._private;
+
+	if (info.class === 'Gene') {
+		return `
+			<div>
+				<span>Symbol: </span><strong>${info.symbol}</strong><br/><div style="padding: 2px"></div>
+				<span>Name: </span><strong>${info.shortName}</strong><br/><div style="padding: 2px"></div>
+				<span>Primary Identifier: </span><strong>${info.name}</strong><br/><div style="padding: 2px"></div>
+				<span>Secondary Identifier: </span><strong>${info.primaryIdentifier}</strong>
+			</div>
+		`;
+	}
+	if (info.class === 'Pathway') {
+		return `
+			<div>
+				<span>Symbol: </span><strong>${node.target[0]._private.data.id}</strong><br/><div style="padding: 4px"></div>
+				<span>Name: </span><strong>${info.name}</strong><br/><div style="padding: 4px"></div>
+			</div>
+		`;
+	}
+}
+
+export {
+	getGraphData,
+	createCytoscapeConfig,
+	createTooltip,
+	createTooltipData
+};
