e6aeb4471d4e3e1d08ae4e7c0c2bc28649d4d06a
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-02 23:07:08 +0530

    added chart test

---
+++ tests/chart.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import imjs from 'imjs';
+import GenePathwayNetwork from '../src/components/GenePathwayNetwork';
+import { queryData } from '../src/query';
+
+describe('charts', () => {
+	let data = [];
+	beforeAll(() => {
+		return queryData({
+			geneId: '128',
+			serviceUrl: 'https://www.humanmine.org/humanmine',
+			imjsClient: imjs
+		})
+			.then(res => (data = res))
+			.catch(() => {});
+	});
+
+	test('GenePathwayNetwork renders canvas', () => {
+		const el = document.createElement('div');
+		ReactDOM.render(<GenePathwayNetwork data={data} />, el);
+		expect(el.innerHTML).toContain('div');
+	});
+});
