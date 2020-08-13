fbf6ae8e380967a275fca2e2219d4d4ab3833cc8
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-08-13 13:03:15 +0530

    added animated loading

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { queryData } from './query';
 import GenePathwayNetwork from './components/GenePathwayNetwork';
 import FilterPanel from './components/FilterPanel';
+import Loading from './components/Loading';
 
 const RootContainer = ({ serviceUrl, entity }) => {
 	const [data, setData] = useState([]);
@@ -130,7 +131,7 @@ const RootContainer = ({ serviceUrl, entity }) => {
 		<div className="rootContainer">
 			<span className="chart-title">Pathway Network</span>
 			{loading ? (
-				<h1>Loading...</h1>
+				<Loading />
 			) : (
 				<div className="innerContainer">
 					<div className="graph">
